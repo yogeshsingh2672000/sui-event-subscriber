fc3950bc0d2952ac9f2c3a57fdf476385ce53439
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2022-11-24 16:56:26 +0530

    bug

--- index.js
+++ index.js
@@ -1,53 +1,33 @@
 const { WebSocket } = require("ws");
+const socket = new WebSocket("ws://localhost:9001");
 
-var JSONRPC_TIMEOUT_MS = 1000;
+const subscribeEvent = async (payload) => {
+  socket.on("open", async () => {
+    console.log("connected to ws");
+    let payload = {
+      jsonrpc: "2.0",
+      id: 1,
+      method: "sui_subscribeEvent",
+      params: [
+        { All: [{ Package: "0x26d943059b2c6420671bc7c66b3e72b8532f0e7f" }] },
+      ],
+    };
+    let result = await socket.send(JSON.stringify(payload));
+    console.log(`response rpc: ${result}`);
 
-var jsonrpc = function (url, onopen, onclose, onnotification) {
-  var rpcid = 0,
-    pending = {},
-    ws = new WebSocket(url);
-  if (!ws) return null;
-  ws.onclose = onclose;
-  ws.onmessage = function (ev) {
-    const frame = JSON.parse(ev.data);
-    console.log("rcvd", frame, "pending:", pending);
-    if (frame.id !== undefined) {
-      if (pending[frame.id] !== undefined) pending[frame.id](frame); // Resolve
-      delete pending[frame.id];
-    } else {
-      if (onnotification) onnotification(frame);
-    }
-  };
-  if (onopen) onopen();
-  return {
-    close: () => ws.close(),
-    call: function (method, params) {
-      const id = rpcid++,
-        request = { id, method, params };
-      ws.send(JSON.stringify(request));
-      console.log("sent", request);
-      return new Promise(function (resolve, reject) {
-        setTimeout(JSONRPC_TIMEOUT_MS, function () {
-          if (pending[id] === undefined) return;
-          log("Timing out frame ", JSON.stringify(request));
-          delete pending[id];
-          reject();
-        });
-        pending[id] = (x) => resolve(x);
-      });
-    },
-  };
+    socket.on("message", (res) => {
+      console.log(`res; ${res}`);
+    });
+  });
 };
 
-jsonrpc(
-  "ws://localhost:9001",
-  () => {
-    console.log("connected");
-  },
-  () => {
-    console.log("disconnected");
-  },
-  (data) => {
-    console.log(`data: ${data}`);
-  }
-);
+const rpcObject = {
+  jsonrpc: "2.0",
+  id: 1,
+  method: "sui_subscribeEvent",
+  params: [
+    { All: [{ Package: "0x26d943059b2c6420671bc7c66b3e72b8532f0e7f" }] },
+  ],
+};
+
+subscribeEvent(rpcObject);
