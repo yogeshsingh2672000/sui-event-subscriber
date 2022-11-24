7ff73d00a31761b9947784fdca8fd0ff92641fce
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2022-11-24 15:59:39 +0530

    bug fix

--- index.js
+++ index.js
@@ -2,20 +2,44 @@
 const { WebSocket } = require("ws");
 // const { JSONRPCClient } = require("json-rpc-2.0");
 let socket = new WebSocket("ws://localhost:9001");
+const payload = {
+  jsonrpc: "2.0",
+  id: 1,
+  method: "sui_subscribeEvent",
+  params: [
+    { All: [{ Package: "0x26d943059b2c6420671bc7c66b3e72b8532f0e7f" }] },
+  ],
+};
+
+// const main = async () => {
+//   socket.on("on", async () => {
+//     console.log("connected to ws");
+//     let res = await socket.emit("rpc", {
+//       jsonrpc: "2.0",
+//       id: 1,
+//       method: "sui_subscribeEvent",
+//       params: [
+//         { All: [{ Package: "0x26d943059b2c6420671bc7c66b3e72b8532f0e7f" }] },
+//       ],
+//     });
+
+//     console.log(res);
+//   });
+// };
+
+// main();
 
 const main = async () => {
-  socket.on("on", async () => {
-    console.log("connected to ws");
-    let res = await socket.emit("rpc", {
-      jsonrpc: "2.0",
-      id: 1,
-      method: "sui_subscribeEvent",
-      params: [
-        { All: [{ Package: "0x26d943059b2c6420671bc7c66b3e72b8532f0e7f" }] },
-      ],
-    });
+  socket.on("connection", (socket) => {
+    // send a message to the client
+    socket.send(JSON.stringify(payload));
 
-    console.log(res);
+    // receive a message from the client
+    socket.on("message", (data) => {
+      const packet = JSON.parse(data);
+
+      console.log(packet);
+    });
   });
 };
 
