5e524362fbfd340981e294f43f43b37ef74ff45d
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2022-11-24 15:45:06 +0530

    socket connected

--- index.js
+++ index.js
@@ -1,38 +1,22 @@
-import fetch from "node-fetch";
-import { JSONRPCClient } from "json-rpc-2.0";
-
-// initializing the app
+// const { WebSocket, createWebSocketStream } = require("ws");
+const { WebSocket } = require("ws");
+// const { JSONRPCClient } = require("json-rpc-2.0");
+let socket = new WebSocket("ws://localhost:9001");
 
 const main = async () => {
-  const URL = "http://127.0.0.1:9001/json";
-  const client = new JSONRPCClient((jsonRPCRequest) =>
-    fetch(URL, {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(jsonRPCRequest),
-    }).then((response) => {
-      if (response.status === 200) {
-        // Use client.receive when you received a JSON-RPC response.
-        return response
-          .json()
-          .then((jsonRPCResponse) => client.receive(jsonRPCResponse));
-      } else if (jsonRPCRequest.id !== undefined) {
-        return Promise.reject(new Error(response.statusText));
-      }
-    })
-  );
-  const payload = {
-    jsonrpc: "2.0",
-    id: 1,
-    method: "sui_subscribeEvent",
-    params: [
-      { All: [{ Package: "0x7bd922a80fcb131a0d6a65485a7c41db28416dd3" }] },
-    ],
-  };
-  client
-    .request("echo", { text: "Hello, World!" })
-    .then((result) => console.log(result));
+  socket.on("open", async () => {
+    console.log("connected to ws");
+    let res = await socket.emit("rpc", {
+      jsonrpc: "2.0",
+      id: 1,
+      method: "sui_subscribeEvent",
+      params: [
+        { All: [{ Package: "0x26d943059b2c6420671bc7c66b3e72b8532f0e7f" }] },
+      ],
+    });
+
+    console.log(res);
+  });
 };
+
 main();
