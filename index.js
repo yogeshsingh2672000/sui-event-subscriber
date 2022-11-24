4fe634807b0c45ec4af3552f3dc16e1b93364468
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2022-11-24 16:28:39 +0530

    bug

--- index.js
+++ index.js
@@ -11,49 +11,48 @@ const payload = {
 };
 let socket = new WebSocket("ws://localhost:9001", payload);
 
-// const main_1 = async () => {
-//   socket.on("on", async () => {
-//     console.log("connected to ws");
-//     let res = await socket.emit("rpc", {
-//       jsonrpc: "2.0",
-//       id: 1,
-//       method: "sui_subscribeEvent",
-//       params: [
-//         { All: [{ Package: "0x26d943059b2c6420671bc7c66b3e72b8532f0e7f" }] },
-//       ],
-//     });
-
-//     console.log(res);
-//   });
-// };
-
-// main_1();
-console.log();
-try {
-  console.log("inside try");
-  const main = async () => {
-    console.log("inside main");
-    socket.on("message", function message(data) {
-      console.log("inside message");
-      console.log("received: %s", data);
+const main_1 = async () => {
+  socket.on("on", async () => {
+    console.log("connected to ws");
+    let res = await socket.emit("rpc", {
+      jsonrpc: "2.0",
+      id: 1,
+      method: "sui_subscribeEvent",
+      params: [
+        { All: [{ Package: "0x26d943059b2c6420671bc7c66b3e72b8532f0e7f" }] },
+      ],
     });
-    // socket.on("connect", () => {
-    //   console.log("inside connect");
-    //   console.log("connected to the socket");
-    //   // send a message to the client
-    //   socket.send(JSON.stringify(payload));
-    //   console.log("payload sent");
 
-    //   // receive a message from the client
-    //   socket.on("message", (data) => {
-    //     const packet = JSON.parse(data);
+    console.log(res);
+  });
+};
 
-    //     console.log(packet);
-    //   });
-    // });
-  };
+main_1();
+// try {
+//   console.log("inside try");
+//   const main = async () => {
+//     console.log("inside main");
+//     socket.on("message", function message(data) {
+//       console.log("inside message");
+//       console.log("received: %s", data);
+//     });
+//     // socket.on("connect", () => {
+//     //   console.log("inside connect");
+//     //   console.log("connected to the socket");
+//     //   // send a message to the client
+//     //   socket.send(JSON.stringify(payload));
+//     //   console.log("payload sent");
 
-  main();
-} catch (error) {
-  console.log(error);
-}
+//     //   // receive a message from the client
+//     //   socket.on("message", (data) => {
+//     //     const packet = JSON.parse(data);
+
+//     //     console.log(packet);
+//     //   });
+//     // });
+//   };
+
+//   main();
+// } catch (error) {
+//   console.log(error);
+// }
