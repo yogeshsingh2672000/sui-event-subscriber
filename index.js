770213a2306fdb7b00003978e537ac1524073e63
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2022-11-24 16:29:34 +0530

    bug

--- index.js
+++ index.js
@@ -9,19 +9,12 @@ const payload = {
     { All: [{ Package: "0x26d943059b2c6420671bc7c66b3e72b8532f0e7f" }] },
   ],
 };
-let socket = new WebSocket("ws://localhost:9001", payload);
+let socket = new WebSocket("ws://localhost:9001");
 
 const main_1 = async () => {
   socket.on("on", async () => {
     console.log("connected to ws");
-    let res = await socket.emit("rpc", {
-      jsonrpc: "2.0",
-      id: 1,
-      method: "sui_subscribeEvent",
-      params: [
-        { All: [{ Package: "0x26d943059b2c6420671bc7c66b3e72b8532f0e7f" }] },
-      ],
-    });
+    let res = await socket.emit("rpc", payload);
 
     console.log(res);
   });
